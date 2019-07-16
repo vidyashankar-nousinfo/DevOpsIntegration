b93465498d87acee02bbccb9fdcd56cf339c1d19
vidyashankar-nousinfo <52958260+vidyashankar-nousinfo@users.noreply.github.com>
2019-07-16 16:33:15 +0530

    Added to GitHub Master

---
+++ DevOpsIntegration/TestCompleteTestProject/My Project/AssemblyInfo.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+<Assembly: AssemblyTitle("TestCompleteTestProject")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("TestCompleteTestProject")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+<Assembly: Guid("d1ead14b-8bc7-4132-bf06-44567c92a264")>
+
+' <Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
